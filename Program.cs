79d1c9b18ed2863b26f2d0773e42853f6171a849
IcoPico <hristoapps@gmail.com>
2016-05-25 22:34:05 +0300

    added program.cs

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine("First");
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Second");
+            }
+            if (!(10 > 20))
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+    }
+}
